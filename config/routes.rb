ce59e8eebb46eea58e90fd62da0fe63ee06c1604
Dileep Nandanam <dileep@Dileeps-MacBook-Air.local>
2019-03-22 23:28:41 -0700

    set root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'exams#index'
   resources :exams do
   	get :random_questions, on: :collection
   	get :progress, on: :collection
